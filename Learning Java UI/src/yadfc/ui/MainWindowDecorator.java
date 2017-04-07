faac4c564c492292503311afe9ddd643bb2faa68
ted.snider <ted.snider@INLAP-WKS881.moserit.com>
2017-04-07 17:29:20 -0400

    Added ViewPaneDecorator for main deployment view
    Added comments to direct further development

--- Learning Java UI/src/yadfc/ui/MainWindowDecorator.java
+++ Learning Java UI/src/yadfc/ui/MainWindowDecorator.java
@@ -25,44 +25,9 @@ public class MainWindowDecorator {
 		stage = mainStage;
 		stage.setTitle("YADFC");
 
-		GridPane grid = new GridPane();
-		grid.setAlignment(Pos.CENTER);
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(25,25,25,25));
+		//TODO: Use BorderPane for main window
+		//TODO: Instantiate and decorate the five panes of the BorderPane
 		
-		Text sceneTitle = new Text("Welcome to YADFC!");
-		sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-		grid.add(sceneTitle, 0, 0, 2, 1);
-		
-		Label userName = new Label("User Name:");
-		grid.add(userName,  0,  1);
-		
-		TextField userTextField = new TextField();
-		grid.add(userTextField, 1, 1);
-		
-		Label pw = new Label("Password:");
-		grid.add(pw, 0, 2);
-		
-		PasswordField pwBox = new PasswordField();
-		grid.add(pwBox, 1, 2);
-		
-		Button btn = new Button("Sign In");
-		HBox hbBtn = new HBox(10);
-		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-		hbBtn.getChildren().add(btn);
-		grid.add(hbBtn, 1, 4);
-		
-		final Text actionTarget = new Text();
-		grid.add(actionTarget, 1, 6);
-		
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				actionTarget.setFill(Color.FIREBRICK);
-				actionTarget.setText("Sign in button pressed");
-			}
-		});
 		Scene scene = new Scene(grid, 300, 275);
 		stage.setScene(scene);
 		
